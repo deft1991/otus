eff2bed4dc48d023e201c7bbb6664c42ce58487b
sgolitsyn <pt.sgolitsyn@prosper.com>
2019-07-07 15:33:02 +0300

    HW-01 add sub-module. add dependencies in pom. build jar with dependencies.

---
+++ otus_2019_06/hw01-maven/src/main/java/ru/deft/homework/HelloOtus.java
@@ -0,0 +1,14 @@
+package ru.deft.homework;
+
+import static com.google.common.math.IntMath.isPowerOfTwo;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(2));
+        System.out.println(isPowerOfTwo(3));
+        System.out.println(isPowerOfTwo(13));
+        System.out.println(isPowerOfTwo(4));
+    }
+
+}
