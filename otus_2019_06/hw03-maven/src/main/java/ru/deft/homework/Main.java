cbb19ff40c2143c553056d7c452262c3f14b44db
sgolitsyn <pt.sgolitsyn@prosper.com>
2019-08-08 11:26:31 +0300

    HW-03 Refactor

--- otus_2019_06/hw03-maven/src/main/java/ru/deft/homework/Main.java
+++ otus_2019_06/hw03-maven/src/main/java/ru/deft/homework/Main.java
@@ -4,9 +4,7 @@ import ru.deft.homework.annotation.After;
 import ru.deft.homework.annotation.Before;
 import ru.deft.homework.annotation.Test;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,33 +12,33 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        Class myClass  = Class.forName("ru.deft.homework.test.TestFirst");
+        Class myClass = Class.forName("ru.deft.homework.test.TestFirst");
         List<String> err = new ArrayList<>();
         Method[] methods = myClass.getMethods();
         List<Method> before = new ArrayList<>();
         List<Method> test = new ArrayList<>();
         List<Method> after = new ArrayList<>();
         for (Method method : methods) {
-            if (method.isAnnotationPresent(Before.class)){
+            if (method.isAnnotationPresent(Before.class)) {
                 before.add(method);
             }
-            if (method.isAnnotationPresent(Test.class)){
+            if (method.isAnnotationPresent(Test.class)) {
                 test.add(method);
             }
-            if (method.isAnnotationPresent(After.class)){
+            if (method.isAnnotationPresent(After.class)) {
                 after.add(method);
             }
         }
 
-        for (Method methodTest : test){
+        for (Method methodTest : test) {
             Object obj = myClass.newInstance();
             invokeMethodList(err, before, obj);
             invokeTestMethods(err, methodTest, obj);
             invokeMethodList(err, after, obj);
         }
-        if (err.isEmpty()){
+        if (err.isEmpty()) {
             System.out.println("Test sucseed");
-        } else  {
+        } else {
             String errors = err.stream().map(String::valueOf).collect(Collectors.joining(", "));
             System.out.println(String.format("Tests finished with %d errors. Errors : %s", err.size(), errors));
         }
