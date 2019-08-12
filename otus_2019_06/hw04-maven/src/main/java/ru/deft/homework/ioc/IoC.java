a0b5b4555a9898e81322bdcc452d93ab8700130c
sgolitsyn <pt.sgolitsyn@prosper.com>
2019-08-12 12:06:50 +0300

    HW-04 Add cache for Log methods

--- otus_2019_06/hw04-maven/src/main/java/ru/deft/homework/ioc/IoC.java
+++ otus_2019_06/hw04-maven/src/main/java/ru/deft/homework/ioc/IoC.java
@@ -6,13 +6,16 @@ import ru.deft.homework.interfaces.PrintInterface;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class IoC {
 
     private static final String LOG_MSG = "Execute method %s with params {%s}";
+    private static List<Method> logableMethods = new ArrayList<>();
 
     public static Object createProxyClass(Class clazz) {
         InvocationHandler printInvocationHandler = new MyLogInvocationHandler(clazz);
@@ -29,17 +32,26 @@ public class IoC {
         }
 
         @Override public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (logableMethods.contains(method)) {
+                printMethod(method, args);
+                return method.invoke(clazz.newInstance(), args);
+            }
             Method proxyMethod = clazz.getDeclaredMethod(method.getName(), method.getParameterTypes());
             Log annotation = proxyMethod.getAnnotation(Log.class);
             if (annotation != null) {
-                System.out.println("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
-                String argsToString = "";
-                if (args != null) {
-                    argsToString = Arrays.stream(args).map(Objects::toString).collect(Collectors.joining(", "));
-                }
-                System.out.println(String.format(LOG_MSG, method.getName(), argsToString));
+                logableMethods.add(method);
+                printMethod(method, args);
             }
             return method.invoke(clazz.newInstance(), args);
+        }
+
+        private void printMethod(Method method, Object[] args) {
+            System.out.println("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+            String argsToString = "";
+            if (args != null) {
+                argsToString = Arrays.stream(args).map(Objects::toString).collect(Collectors.joining(", "));
+            }
+            System.out.println(String.format(LOG_MSG, method.getName(), argsToString));
         }
     }
 
