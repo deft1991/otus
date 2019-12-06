e478edb367b5811a7e1f36f86ac061c07f3e59a9
sgolitsyn <pt.sgolitsyn@prosper.com>
2019-12-06 14:21:41 +0300

    HW16-MAVEN Init commit

---
+++ otus_2019_06/hw16-maven/src/test/java/ru/deft/hw16maven/Hw16MavenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.deft.hw16maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw16MavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
