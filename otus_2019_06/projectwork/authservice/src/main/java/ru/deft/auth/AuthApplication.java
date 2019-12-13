c06653315246a49441825f85725656c633219629
sgolitsyn <pt.sgolitsyn@prosper.com>
2019-12-13 10:26:32 +0300

    authservice Add auth via data base. Add jwt token for login

---
+++ otus_2019_06/projectwork/authservice/src/main/java/ru/deft/auth/AuthApplication.java
@@ -0,0 +1,24 @@
+package ru.deft.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+@RestController
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+    @RequestMapping(value = "/products")
+    public String getProductName() {
+        return "Honey";
+    }
+}
